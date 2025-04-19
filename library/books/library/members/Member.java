0f32021b98509e9aa7abb052331e3e456b2a03c9
KRISHNA666 <113873743+KRSHNA666@users.noreply.github.com>
2025-04-19 22:58:11 +0530

    Create Member.java

---
+++ library/books/library/members/Member.java
@@ -0,0 +1,44 @@
+package library.members;
+
+import library.books.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    private String name;
+    private String memberId;
+    private List<Book> borrowedBooks;
+
+    public Member(String name, String memberId) {
+        this.name = name;
+        this.memberId = memberId;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    public void displayBorrowedBooks() {
+        System.out.println(name + " has borrowed:");
+        for (Book book : borrowedBooks) {
+            System.out.println(" - " + book);
+        }
+    }
+}
